14f43b471d94172fbf31d9fcf67b1442468e91d9
Oroppo <dante.arruda416@gmail.com>
2021-11-03 20:09:54 -0400

    Update MouseController.cpp

--- OTTER/projects/Week8-Completed/src/Gameplay/Components/MouseController.cpp
+++ OTTER/projects/Week8-Completed/src/Gameplay/Components/MouseController.cpp
@@ -20,9 +20,8 @@ void MouseController::Awake()
         glfwSetInputMode(GetGameObject()->GetScene()->Window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
         glfwSetInputMode(GetGameObject()->GetScene()->Window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     }
+
     SetLastFrame(glm::vec2(0, 0));
-
-
 }
 
  
@@ -52,8 +51,13 @@ void MouseController::Update(float deltaTime) {
     glfwGetCursorPos(GetGameObject()->GetScene()->Window, &xPos, &yPos);
     glfwGetWindowSize(GetGameObject()->GetScene()->Window, &windowWidth, &windowHeight);
 
+    //glfw wants doubles and glm::vec2 wants floats, 
+    //casting is just messy to read so we're doing a quick type swap here:
     float x = xPos;
+    if (x = 0.f)x = 0.001f;
     float y = yPos;
+    if (y = 0.f)y = 0.001f;
+    
     
     SetThisFrame(glm::vec2(x, y));
 
@@ -62,23 +66,42 @@ void MouseController::Update(float deltaTime) {
     glm::vec2 temp(thisFrame * thisFrame + lastFrame * lastFrame);
     glm::vec2 normalized(temp/temp);
 
-    float thisMagnitude = (thisFrame.x + thisFrame.y) / (thisFrame.x + thisFrame.y);
-    float lastMagnitude = (lastFrame.x + lastFrame.y) / (lastFrame.x + lastFrame.y);
+    //Calculate X and Y Distance
+    float DistanceX = (thisFrame.x - lastFrame.x);
+    if (DistanceX = 0.f) DistanceX = 0.001f;
+    float DistanceY = (thisFrame.y - lastFrame.y);
+    if (DistanceY = 0.f) DistanceY = 0.001f;
 
+    //Divide By window height/width to make the movement proportional to % of screen moved
+    DistanceX /= windowWidth;
+    DistanceY /= windowHeight;
 
-    normalized *=1;
+    //std::cout << "Cursor: " << x << ", " << y << std::endl;
 
-    GetThisFrame();
-    GetLastFrame();
+    //sets position equal to current position times the normalized Direction
+    //over the distance between 2 frames
 
-    
+    GetGameObject()->SetPostion(GetGameObject()->GetPosition() + 
+        glm::vec3(normalized.x* DistanceX, normalized.y* -DistanceY, 0.0f));
 
-    std::cout << "Cursor: " << x << ", " << y << std::endl;
+    glm::vec3 CurrentPosition = GetGameObject()->GetPosition();
 
+    //Boundary Cheking since Kinematic Objects Don't Interact with colliders traditionally
 
-    GetGameObject()->SetPostion(GetGameObject()->GetPosition() + glm::vec3(normalized, 0.0f));
+    if (GetGameObject()->GetPosition().x < 1.565) {
+        GetGameObject()->SetPostion(glm::vec3(1.565f, CurrentPosition.y, CurrentPosition.z));
+    }
+    if (GetGameObject()->GetPosition().x > 2.25) {
+        GetGameObject()->SetPostion(glm::vec3(2.25f, CurrentPosition.y, CurrentPosition.z));
+    }
+    if (GetGameObject()->GetPosition().y > 0.4) {
+        GetGameObject()->SetPostion(glm::vec3(CurrentPosition.x, 0.4f, CurrentPosition.z));
+    }
+    if (GetGameObject()->GetPosition().y < -0.4) {
+        GetGameObject()->SetPostion(glm::vec3(CurrentPosition.x, -0.4f, CurrentPosition.z));
+    }
 
-
-   SetLastFrame(thisFrame);
+    //this frame last frame
+    SetLastFrame(thisFrame);
 }
 
