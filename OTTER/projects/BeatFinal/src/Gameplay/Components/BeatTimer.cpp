ae6470d81faa80641d03566305c42b12ab9e2fa7
Ryan Fieldhouse <rcfieldhouse@yahoo.com>
2021-12-05 16:01:48 -0500

    added beat timer

---
+++ OTTER/projects/BeatFinal/src/Gameplay/Components/BeatTimer.cpp
@@ -0,0 +1,32 @@
+#include "Gameplay/Components/BeatTimer.h"
+#include <GLFW/glfw3.h>
+#include "Gameplay/GameObject.h"
+#include "Gameplay/Scene.h"
+#include "Utils/ImGuiHelper.h"
+
+void BeatTimer::Awake()
+{
+
+}
+
+void BeatTimer::RenderImGui() {
+
+}
+
+
+BeatTimer::BeatTimer() :
+	IComponent(){}
+
+
+
+
+
+BeatTimer::~BeatTimer() = default;
+
+
+
+void BeatTimer::Update(float deltaTime) {
+
+
+}
+
